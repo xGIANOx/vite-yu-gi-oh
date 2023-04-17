6227392ce7de9683332e1dd103569ba405ac9a84
xGIANOx <studioenricogian@gmail.com>
2023-04-17 17:17:29 +0200

    [fix] style

--- src/components/CardsComponent.vue
+++ src/components/CardsComponent.vue
@@ -15,7 +15,7 @@ export default {
 </script>
 
 <template>
-  <div class="cardsComponent row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-5 mt-5 pb-5 g-4">
+  <div class="cardsComponent row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-5 mt-5 pb-5 g-4 px-3">
     <CardItem v-if="state.cards != []" v-for="card in state.cards" :card="card"></CardItem>
   </div>
 </template>
